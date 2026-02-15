4dc473cc9014c877e0c1a5b5dc54e84a83ec5824
adr4an <adr4antoh@gmail.com>
2026-02-15 09:07:16 +0800

    conflict 2

--- MyClass.java
+++ MyClass.java
@@ -3,7 +3,5 @@ public class MyClass {
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Hello, Wurddd!");
-        System.out.println("Hello, Nga!");
     }
 }
